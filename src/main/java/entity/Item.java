e6b71ddcefbb292f7b2623934354b8819be9755a
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-10 16:18:43 +0000

    Items can have a rule id

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -3,14 +3,20 @@ package entity;
 public class Item {
     private final String sku;
     private final double unitPrice;
+    private final Integer ruleId;
 
-    private Item(String sku, double unitPrice) {
+
+    private Item(String sku, double unitPrice, Integer ruleId) {
         this.sku = sku;
         this.unitPrice = unitPrice;
+        this.ruleId = ruleId ;
     }
 
     public static Item create(String sku, double price) {
-        return new Item(sku, price);
+        return new Item(sku, price, null);
+    }
+    public static Item create(String sku, double price, int ruleId) {
+        return new Item(sku, price, ruleId);
     }
 
     public String getSku() {
@@ -21,4 +27,7 @@ public class Item {
         return unitPrice;
     }
 
+    public int getRuleId() {
+        return ruleId;
+    }
 }
--- src/test/java/entity/ItemTest.java
+++ src/test/java/entity/ItemTest.java
@@ -7,10 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ItemTest {
 
     @Test
-    void create_an_item_with_valid_properties() {
+    void create_an_item_without_ruleId() {
         Item item = Item.create("A", 50.0);
         assertEquals(item.getSku(), "A");
         assertEquals(item.getUnitPrice(), 50.0);
+    }
+
+    @Test
+    void create_an_item_with_ruleId() {
+        Item item = Item.create("A", 50.0, 1);
+        assertEquals(item.getSku(), "A");
+        assertEquals(item.getUnitPrice(), 50.0);
+        assertEquals(item.getRuleId(), 1);
     }
 
 }
