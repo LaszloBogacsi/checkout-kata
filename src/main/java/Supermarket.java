7a706791dcd6c22178dc2ba5a4bc6b759ce723ea
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-11 21:26:07 +0000

    Supermarket class with a main method to experiment with the checkout

---
+++ src/main/java/Supermarket.java
@@ -0,0 +1,42 @@
+import entity.Item;
+import entity.Rule;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Supermarket object representing a supermarket with checkouts and items and pricing rules
+ */
+public class Supermarket {
+    /**
+     * Main method to try the checkout system
+     * Below find a set of rules and items from the kata description
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        // a list of rules that can apply to items
+        List<Rule> rules = Arrays.asList(
+                Rule.create(1, "multiBuy", 2, 45.0),
+                Rule.create(2,"multiBuy", 3, 130.0));
+
+        // Inventory of items some with defined associated rules
+        Item itemA = Item.create("A", 50, 2);
+        Item itemB = Item.create("B", 30, 1);
+        Item itemC = Item.create("C", 20);
+        Item itemD = Item.create("D", 15);
+
+        Checkout checkout = Checkout.create(rules);
+
+        checkout.scan(itemB);
+        checkout.scan(itemA);
+        checkout.scan(itemA);
+        checkout.scan(itemB);
+        checkout.scan(itemA);
+        checkout.scan(itemC);
+        checkout.scan(itemD);
+
+
+        System.out.println("Checkout: " + checkout);
+        System.out.println("Total price: " + checkout.getTotal());
+    }
+}
