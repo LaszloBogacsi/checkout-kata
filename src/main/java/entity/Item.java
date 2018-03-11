671a1f94dd47a5f535f397fed8b8b33e73e66943
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-11 21:23:28 +0000

    Add documentation

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -1,33 +1,77 @@
 package entity;
 
+/**
+ * Item entity to describe an item in the supermarket
+ */
 public class Item {
-    private final String sku;
-    private final double unitPrice;
-    private final Integer ruleId;
+    private final String sku; // stock keeping unit
+    private final double unitPrice; // price of one item
+    private final Integer ruleId; // "foreign key" for an associated rule that applies to this item
 
 
+    /**
+     * private constructor to set the properties of an item
+     * @param sku string stock keeping unit
+     * @param unitPrice double price of one item
+     * @param ruleId Integer associated rule id
+     */
     private Item(String sku, double unitPrice, Integer ruleId) {
         this.sku = sku;
         this.unitPrice = unitPrice;
         this.ruleId = ruleId ;
     }
 
+    /**
+     * Factory method to create an item with 2 params
+     * @param sku string stock keeping unit identifier
+     * @param price double price of an item
+     * @return a new Item with the properies as above
+     */
     public static Item create(String sku, double price) {
         return new Item(sku, price, null);
     }
+
+    /**
+     * Factory method to create an item with 3 params
+     * @param sku string stock keeping unit identifier
+     * @param price double price of an item
+     * @param ruleId Integer associated rule id that applies to this item
+     * @return a new Item with the properties as above
+     */
     public static Item create(String sku, double price, int ruleId) {
         return new Item(sku, price, ruleId);
     }
 
+    /**
+     * Getter for sku
+     * @return a string sku
+     */
     public String getSku() {
         return sku;
     }
 
+    /**
+     * Getter for unit price
+     * @return a double price
+     */
     public double getUnitPrice() {
         return unitPrice;
     }
 
+    /**
+     * Getter for rule id
+     * @return int rule id
+     */
     public int getRuleId() {
         return ruleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "sku='" + sku + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", ruleId=" + ruleId +
+                '}';
     }
 }
--- src/main/java/entity/Rule.java
+++ src/main/java/entity/Rule.java
@@ -1,11 +1,21 @@
 package entity;
 
+/**
+ * Rule entity to describe a pricing rule
+ */
 public class Rule {
-    private final int ruleId;
-    private final String type;
-    private final int minQuantityToQualify;
-    private final double newPrice;
+    private final int ruleId; // unique identifier of a rule
+    private final String type; // rule type (multibuy... etc)
+    private final int minQuantityToQualify; // int number of minimum items to qualify to the rule
+    private final double newPrice; // new offer price of the items
 
+    /**
+     * Constructor to set the properties of a rule
+     * @param ruleId int unique identifier
+     * @param type string rule type
+     * @param minQuantityToQualify int number of items to qualify to the rule
+     * @param newPrice double new offer price
+     */
     private Rule(int ruleId, String type, int minQuantityToQualify, double newPrice) {
         this.ruleId = ruleId;
         this.type = type;
@@ -13,23 +23,57 @@ public class Rule {
         this.newPrice = newPrice;
     }
 
+    /**
+     * Static api method to create a new rule with all parameters
+     * @param ruleId int unique identifier
+     * @param type string rule type
+     * @param minQuantityToQualify int number of items to qualify to the rule
+     * @param newPrice double new offer price
+     * @return a new Rule object
+     */
     public static Rule create(int ruleId, String type, int minQuantityToQualify, double newPrice) {
         return new Rule(ruleId, type, minQuantityToQualify, newPrice);
     }
 
+    /**
+     * Getter for rule Id
+     * @return int rule id
+     */
     public int getRuleId() {
         return ruleId;
     }
 
+    /**
+     * Getter for the rule type
+     * @return strung type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Getter
+     * @return int minimum number number of items to qualify
+     */
     public int getMinQuantityToQualify() {
         return minQuantityToQualify;
     }
 
+    /**
+     * Getter for the new special price
+     * @return double special price
+     */
     public double getNewPrice() {
         return newPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Rule{" +
+                "ruleId=" + ruleId +
+                ", type='" + type + '\'' +
+                ", minQuantityToQualify=" + minQuantityToQualify +
+                ", newPrice=" + newPrice +
+                '}';
     }
 }
