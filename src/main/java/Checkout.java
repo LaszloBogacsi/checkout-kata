10092bf228dbc27594c5014bdf3442d8daa2cbbd
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-11 21:22:48 +0000

    Add "create" factory methods to checkout Add documentation

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -6,53 +6,126 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Checkout to calculate total price for a number of items
+ */
 public class Checkout {
-    private List<Item> basket = new ArrayList<>();
-    private double total = 0.0;
-    private final List<Rule> rules;
+    private List<Item> basket = new ArrayList<>(); // basket is a list of items added to the checkout
+    private double total = 0.0; // the total price of the basket
+    private final List<Rule> rules; // list of pricing rules
 
-    public Checkout(List<Rule> rules) {
+    /**
+     * Cnstructor to instantiate the checkout with a set of rules
+     * @param rules are a list of multibuy pricing rules list can be empty or null
+     */
+    private Checkout(List<Rule> rules) {
         this.rules = rules;
     }
 
+    /**
+     * Factory method to create a checkout object
+     * @param rules a list of multibuy rules can be empty
+     * @return a checkout object with rules
+     */
+    public static Checkout create(List<Rule> rules) {
+        return new Checkout(rules);
+    }
+
+    /**
+     * Factory method to create a checkout object without rules
+     * @return a checkout object with null rules
+     */
+    public static Checkout create() {
+        return new Checkout(null);
+    }
+
+    /**
+     * To get total price of a basket
+     * @return double total price of the basket
+     */
     public double getTotal() {
         return total;
     }
 
+    /**
+     * The basket contains the scanned items.
+     * @return a list of items
+     */
     public List<Item> getBasket() {
         return basket;
     }
 
+    /**
+     * Adds an item to a basket
+     * Applies the pricing rule
+     * Updates the total price
+     * @param item to add to the basket
+     */
     public void scan(Item item) {
         basket.add(item);
+        log("INFO", "item added: " + item);
         applyMultibuyRule(this.basket);
         updateTotal(this.basket);
+        log("INFO", "total price: " + this.total);
     }
 
+    /**
+     * Sums the prices of the passed in basket and updates the total with tbe new value.
+     * @param basket a list of items already scanned
+     */
     private void updateTotal(List<Item> basket) {
         this.total = basket.stream()
                 .mapToDouble(Item::getUnitPrice)
                 .sum();
     }
 
+    /**
+     * Applies the list of rules to the items in the basket by grabbing the rule for the last item added to the basket,
+     * if any, and based on counting the matching items in the basket can determine if the rule should be applied.
+     * If should be applied than creates a new Item with the rule type as sku and the discount (price difference) as a
+     * negative double price and adds this discount item to the basket.
+     * @param basket list of items
+     */
     private void applyMultibuyRule(List<Item> basket) {
         Item lastItem = basket.get(basket.size()-1);
         int lastItemCount = Collections.frequency(basket, lastItem);
-        if (lastItemCount > 1 && this.rules.size() > 0) {
+        if (lastItemCount > 1 && this.rules != null && this.rules.size() > 0) {
             getRuleById(lastItem.getRuleId()).ifPresent(rule -> {
                 if (lastItemCount % rule.getMinQuantityToQualify() == 0) {
                     double discount = rule.getNewPrice() - lastItem.getUnitPrice() * rule.getMinQuantityToQualify();
                     this.basket.add(Item.create(rule.getType(), discount));
+                    log("INFO", "discount applied:  " + discount);
+
                 }
             });
-
         }
     }
 
+    /**
+     * Helper method to grab a rule from the rules list by it's id
+     * @param id int rule id
+     * @return an optional rule when the rule exists in the rules list, optional empty otherwise.
+     */
     private Optional<Rule> getRuleById(int id) {
        return this.rules.stream()
                .filter(rule -> rule.getRuleId() == id)
                .findFirst();
     }
 
+    /**
+     * Helper method to log events to the standard output
+     * @param level string level of a log message
+     * @param message string body of a log message
+     */
+    private void log(String level, String message) {
+        System.out.println("[" + level + "] " + message);
+    }
+
+    @Override
+    public String toString() {
+        return "Checkout{" +
+                "number of items in the basket=" + basket.size() +
+                ", current total=" + total +
+                '}';
+    }
 }
--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -21,37 +21,49 @@ class CheckoutTest {
         rules = new ArrayList<>();
     }
 
+    // No rules passed in
     @Test
     void total_is_0_on_init() {
-     checkout = new Checkout(rules);
+     checkout = Checkout.create();
      assertEquals(checkout.getTotal(), 0);
     }
 
     @Test
     void basket_is_empty_on_init() {
-        checkout = new Checkout(rules);
+        checkout = Checkout.create();
         assertTrue(checkout.getBasket().isEmpty());
     }
 
     @Test
     void can_add_one_item_to_the_basket() {
-        checkout = new Checkout(rules);
+        checkout = Checkout.create();
         checkout.scan(itemA);
         assertEquals(checkout.getBasket().size(), 1);
     }
 
     @Test
     void can_calculate_total() {
-        checkout = new Checkout(rules);
+        checkout = Checkout.create();
         checkout.scan(itemA);
         checkout.scan(itemA);
         assertEquals(checkout.getTotal(), 2.0);
     }
 
     @Test
+    void can_handle_when_no_rules_passed_in_but_items_are_assigned_to_rules() {
+        checkout = Checkout.create();
+        checkout.scan(itemB);
+        checkout.scan(itemB);
+        checkout.scan(itemC);
+        checkout.scan(itemC);
+        assertEquals(checkout.getTotal(), 10.0);
+    }
+
+    // with rules
+    @Test
     void can_apply_one_rule() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemB);
         assertEquals(checkout.getTotal(), 3.0);
@@ -60,7 +72,7 @@ class CheckoutTest {
     @Test
     void can_apply_one_rule_twice() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemB);
         checkout.scan(itemB);
@@ -71,7 +83,7 @@ class CheckoutTest {
     @Test
     void can_apply_one_rule_when_items_not_in_order() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemA);
         checkout.scan(itemB);
@@ -82,7 +94,7 @@ class CheckoutTest {
     void can_apply_multiple_rules() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
         rules.add(Rule.create(2, "testType", 2, 4.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemB);
         checkout.scan(itemC);
@@ -94,7 +106,7 @@ class CheckoutTest {
     void can_apply_multiple_rules_when_items_not_in_order() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
         rules.add(Rule.create(2, "testType", 2, 4.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemC);
         checkout.scan(itemB);
@@ -107,7 +119,7 @@ class CheckoutTest {
     void can_apply_multiple_rules_when_items_not_in_order_and_some_has_no_rule() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
         rules.add(Rule.create(2, "testType", 2, 4.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemB);
         checkout.scan(itemC);
         checkout.scan(itemA);
@@ -119,7 +131,7 @@ class CheckoutTest {
     @Test
     void can_operate_when_there_is_no_matching_rule_present() {
         rules.add(Rule.create(1, "testType", 2, 3.0));
-        checkout = new Checkout(rules);
+        checkout = Checkout.create(rules);
         checkout.scan(itemC); // no matching rule for itemC
         checkout.scan(itemC);
         assertEquals(checkout.getTotal(), 6.0);
